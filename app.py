ff15e5c505e3f337bac457eabbf0ab76eb60e9e1
Nigar-Fatima-7 <78277233+Nigar-Fatima-7@users.noreply.github.com>
2022-02-04 00:47:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,6 @@ def predict_dia():
     return render_template('dataset.html', predict = f'the person is {ans}')
 
 #run the app
-app.run(debug=True)
+if __name__= '__main__':
+    app.run(debug=True)
 
